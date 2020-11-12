5d55ebe579ef41e6028a2b4edf9bba81ecf02e21
Alex Berrian <alex.berrian@gmail.com>
2020-11-12 14:53:10 -0800

    Adding Dockersetup.py with fixed package versions, == not >=

---
+++ Dockersetup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='JamInTune-alexberrian',
+     version='0.0.0',
+     description='Tune an audio recording to match standard Western piano key frequencies',
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     author='Alex Berrian',
+     author_email='ajberrian@ucdavis.edu',
+     url='https://www.github.com/alexberrian/JamInTune/',
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: BSD 3-Clause \"New\" or \"Revised\" License",
+         "Operating System :: OS Independent",
+     ],
+     python_requires='>=3.7',
+     install_requires=['numpy==1.16.4',
+                       'librosa==0.8.0',
+                       'soundfile==0.10.3.post1',
+                       'weightedstats==0.4.1',
+                       'sox==1.4.1',
+                       'llvmlite==0.33.0',
+                       'numba==0.50.1',
+                       ],  #Fix llvmlite and numba because they and librosa don't play well together
+)
